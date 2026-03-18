723c84b19255857682958b04082d6ad7d57a22b0
heng <alivetaxi@gmail.com>
2026-03-18 22:23:13 +0800

    feat: add Firestore rules and composite indexes for album queries

--- infra/modules/firestore/main.tf
+++ infra/modules/firestore/main.tf
@@ -16,3 +16,54 @@ resource "google_firestore_database" "default" {
 
   delete_protection_state = "DELETE_PROTECTION_ENABLED"
 }
+
+# Composite indexes required by the albums list queries (where + order_by on different fields).
+
+resource "google_firestore_index" "albums_owner_updated" {
+  project    = var.project_id
+  database   = google_firestore_database.default.name
+  collection = "albums"
+
+  fields {
+    field_path = "ownerId"
+    order      = "ASCENDING"
+  }
+  fields {
+    field_path = "updatedAt"
+    order      = "DESCENDING"
+  }
+}
+
+resource "google_firestore_index" "albums_visibility_updated" {
+  project    = var.project_id
+  database   = google_firestore_database.default.name
+  collection = "albums"
+
+  fields {
+    field_path = "visibility"
+    order      = "ASCENDING"
+  }
+  fields {
+    field_path = "updatedAt"
+    order      = "DESCENDING"
+  }
+}
+
+resource "google_firestore_index" "albums_visibility_group_updated" {
+  project    = var.project_id
+  database   = google_firestore_database.default.name
+  collection = "albums"
+
+  fields {
+    field_path = "visibility"
+    order      = "ASCENDING"
+  }
+  fields {
+    field_path = "groupId"
+    order      = "ASCENDING"
+  }
+  fields {
+    field_path = "updatedAt"
+    order      = "DESCENDING"
+  }
+}
